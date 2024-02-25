d2a6cc96adaec1cd924c6198ba846f1b95fa7d39
Liliana <lilianafinona3@gmail.com>
2024-02-25 08:19:23 -0600

    Updated resources.html
    Differentiated speak functions.
    lm

--- https.js
+++ https.js
@@ -1,4 +1,4 @@
-function speak() {
+function speakH() {
     // Create a SpeechSynthesisUtterance
     const utterance = new SpeechSynthesisUtterance("Secure and safe websites will start their website with https colon: which shows they are on a secure protocol. Check this url to see that HTTPS is a part of it!");
     // Select a voice
--- resources_start.js
+++ resources_start.js
@@ -1,4 +1,4 @@
-function speak() {
+function speakR() {
     // Create a SpeechSynthesisUtterance
     const utterance = new SpeechSynthesisUtterance("Most banks have website options to do basic things like check how much money is still in an account, but there are some common signs to see if a web page is unsafe. ");
     // Select a voice
